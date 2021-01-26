64b8af9830820357ea43e8a5ce0cd74ff09898b2
fibo <fibo@users.noreply.github.com>
2021-01-27 00:21:56 +0100

    add window-frame component

--- README.md
+++ README.md
@@ -1,6 +1,32 @@
 # infinite-canvas
 
-> is a web component
+> is a Web Component
+
+## `<infinite-canvas>`
+
+HTML attributes:
+
+* *readonly*: `Boolean`
+
+CSS variables:
+
+* *--infinite-canvas-background-color*: `#fefefe`
+* *--infinite-canvas-width*: `100vw`
+* *--infinite-canvas-height*: `100vh`
+
+## `<window-frame>`
+
+HTML attributes:
+
+* *top*: `Number`
+* *left*: `Number`
+* *width*: `Number`
+* *height*: `Number`
+* *src*: `String`
+
+CSS variables:
+
+* *--window-frame-box-shadow-color*: `rgba(0, 0, 0, 0.17)`
 
 ## License
 
--- infinite-canvas.js
+++ infinite-canvas.js
@@ -7,17 +7,21 @@ constructor() {
 
   const template = document.createElement('template');
   template.innerHTML = `
-  <style>
-    :host {
-      background-color: var(--infinite-canvas-background-color, #fefefe);
-      position: absolute;
-      left: 0;
-      top: 0;
-      width: 100vw;
-      height: 100vh;
-    }
-  </style>
-  `;
+<style>
+  :host {
+    background-color: var(--infinite-canvas-background-color, #fefefe);
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: var(--infinite-canvas-width, 100vw);
+    height: var(--infinite-canvas-width, 100vh);
+  }
+  ::slotted(window-frame) {
+    position: absolute;
+  }
+</style>
+<slot></slot>
+`;
 
   this.attachShadow({mode: 'open'}).appendChild(template.content.cloneNode(true));
 }
@@ -30,18 +34,18 @@ attributeChangedCallback(name, oldValue, newValue) {
 }
 
 connectedCallback() {
-  this.addEventListener('pointerdown', this.handlePointerdown);
-  this.addEventListener('pointermove', this.handlePointermove);
-  this.addEventListener('pointerup', this.handlePointerup);
+  this.addEventListener('pointerdown', this.onPointerdown);
+  this.addEventListener('pointermove', this.onPointermove);
+  this.addEventListener('pointerup', this.onPointerup);
 }
 
 disconnectedCallback() {
-  this.removeEventListener('pointerdown', this.handlePointerdown);
-  this.removeEventListener('pointermove', this.handlePointermove);
-  this.removeEventListener('pointerup', this.handlePointerup);
+  this.removeEventListener('pointerdown', this.onPointerdown);
+  this.removeEventListener('pointermove', this.onPointermove);
+  this.removeEventListener('pointerup', this.onPointerup);
 }
 
-handlePointerdown(event) {
+onPointerdown(event) {
   const { clientX, clientY } = event;
   const x = Math.round(clientX);
   const y = Math.round(clientY);
@@ -50,7 +54,7 @@ handlePointerdown(event) {
   this.pointerCoordinates = { x, y };
 }
 
-handlePointermove(event) {
+onPointermove(event) {
   const {
     isDragging,
     origin = { x: 0, y: 0 },
@@ -71,19 +75,80 @@ handlePointermove(event) {
   }
 }
 
-handlePointerup() {
+onPointerup() {
   this.isDragging = false;
 }
 
 get readonly() {
   return this.hasAttribute('readonly')
 }
-
 set readonly(value) {
-  if (value) {
+  const isReadonly = Boolean(value);
+  if (isReadonly) {
     this.setAttribute('readonly', '')
   } else {
     this.removeAttribute('readonly')
   }
+}
+});
+
+/**
+ * WindowFrame
+ */
+customElements.define('window-frame', class WindowFrame extends HTMLElement {
+constructor() {
+  super();
+
+  const template = document.createElement('template');
+  template.innerHTML = `
+<style>
+  :host {
+    box-shadow: 1px 1px 7px 1px var(
+      --window-frame-box-shadow-color, rgba(0, 0, 0, 0.17)
+    );
+  }
+</style>
+
+<iframe width="375" height="812" frameborder="0" src="pages/welcome.html"></iframe>
+`;
+
+  this.attachShadow({mode: 'open'}).appendChild(template.content.cloneNode(true));
+}
+
+static get observedAttributes() {
+  return ['top', 'left', 'width', 'height', 'src'];
+}
+
+attributeChangedCallback(name, oldValue, newValue) {
+  switch (name) {
+    case 'top':
+    case 'left': {
+      const num = Math.round(newValue);
+
+      if (typeof num === 'number') {
+        this.style[name] = `${num}px`;
+      }
+
+      break;
+    }
+    case 'width':
+    case 'height': {
+      const num = Math.round(newValue);
+
+      if (typeof num === 'number' && num >= 0) {
+        this.iframe[name] = num;
+      }
+
+      break;
+    }
+    case 'src': {
+      this.iframe.src = newValue;
+      break;
+    }
+  }
+}
+
+get iframe() {
+  return this.shadowRoot.querySelector('iframe');
 }
 });
