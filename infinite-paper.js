3b005f688cea050ba007a677fbb610d92dc6264a
fibo <fibo@users.noreply.github.com>
2024-12-30 00:33:43 +0100

    use tabs

--- infinite-paper.js
+++ infinite-paper.js
@@ -1,12 +1,12 @@
 /**
  * InfinitePaper
  */
-customElements.define('infinite-paper', class InfinitePaper extends HTMLElement {
-constructor() {
-  super();
+class InfinitePaper extends HTMLElement {
+	constructor() {
+		super();
 
-  const template = document.createElement('template');
-  template.innerHTML = `
+		const template = document.createElement("template");
+		template.innerHTML = `
 <style>
   :host {
     background-color: var(--infinite-paper-background-color, #fefefe);
@@ -23,132 +23,140 @@ constructor() {
 <slot></slot>
 `;
 
-  this.attachShadow({mode: 'open'}).appendChild(template.content.cloneNode(true));
+		this.attachShadow({mode: "open"}).appendChild(
+			template.content.cloneNode(true),
+		);
+	}
+
+	static get scalePrecision() {
+		return 3;
+	}
+
+	static get observedAttributes() {
+		return ["scale"];
+	}
+
+	attributeChangedCallback(name, oldValue, newValue) {
+		switch (name) {
+			case "scale": {
+				this.windowFrames.forEach((element) => {
+					const wantedWidth = element.getAttribute("width");
+					const wantedHeight = element.getAttribute("height");
+
+					const width = Math.round(wantedWidth * newValue);
+					const height = Math.round(wantedHeight * newValue);
+
+					element.style.width = `${width}px`;
+					element.style.height = `${height}px`;
+
+					const iframe = element.shadowRoot.querySelector("iframe");
+					iframe.style.transform = `scale(${newValue})`;
+				});
+				break;
+			}
+		}
+	}
+
+	connectedCallback() {
+		// Disable scroll.
+		document.body.style.overflow = "hidden";
+
+		this.addEventListener("pointerdown", this.onPointerdown);
+		this.addEventListener("pointerleave", this.onPointerleave);
+		this.addEventListener("pointermove", this.onPointermove);
+		this.addEventListener("pointerup", this.onPointerup);
+		this.addEventListener("wheel", this.onWheel);
+	}
+
+	disconnectedCallback() {
+		this.removeEventListener("pointerdown", this.onPointerdown);
+		this.removeEventListener("pointerleave", this.onPointerleave);
+		this.removeEventListener("pointermove", this.onPointermove);
+		this.removeEventListener("pointerup", this.onPointerup);
+	}
+
+	onPointerdown(event) {
+		const {clientX, clientY} = event;
+		const x = Math.round(clientX);
+		const y = Math.round(clientY);
+
+		this.isDragging = true;
+		this.pointerCoordinates = {x, y};
+	}
+
+	onPointermove(event) {
+		const {
+			isDragging,
+			origin = {x: 0, y: 0},
+			pointerCoordinates,
+		} = this;
+		const {clientX, clientY} = event;
+		const x = Math.round(clientX);
+		const y = Math.round(clientY);
+
+		if (isDragging) {
+			// Translate origin.
+			this.origin = {
+				x: origin.x - pointerCoordinates.x + x,
+				y: origin.y - pointerCoordinates.y + y,
+			};
+			// Apply to children nodes.
+			this.windowFrames.forEach((element) => {
+				element.style.left = `${this.origin.x}px`;
+				element.style.top = `${this.origin.y}px`;
+			});
+			// Update pointer coordinates.
+			this.pointerCoordinates = {x, y};
+		}
+	}
+
+	onPointerleave() {
+		this.stopDragging();
+	}
+
+	onPointerup() {
+		this.stopDragging();
+	}
+
+	onWheel(event) {
+		this.scale =
+			this.scale -
+			event.deltaY * Math.pow(10, -InfinitePaper.scalePrecision);
+	}
+
+	stopDragging() {
+		this.isDragging = false;
+	}
+
+	get windowFrames() {
+		return this.querySelectorAll(":scope > window-frame");
+	}
+
+	get scale() {
+		return Number(this.getAttribute("scale")) || 1;
+	}
+
+	set scale(value) {
+		if (value > 0) {
+			this.setAttribute(
+				"scale",
+				Number(value.toFixed(InfinitePaper.scalePrecision)),
+			);
+		}
+	}
 }
 
-static get scalePrecision () {
-  return 3;
-}
-
-static get observedAttributes() {
-  return ['scale'];
-}
-
-attributeChangedCallback(name, oldValue, newValue) {
-  switch (name) {
-    case 'scale': {
-      this.windowFrames.forEach((element) => {
-        const wantedWidth = element.getAttribute('width');
-        const wantedHeight = element.getAttribute('height');
-
-        const width = Math.round(wantedWidth * newValue);
-        const height = Math.round(wantedHeight * newValue);
-
-        element.style.width = `${width}px`;
-        element.style.height = `${height}px`;
-
-        const iframe = element.shadowRoot.querySelector('iframe');
-        iframe.style.transform = `scale(${newValue})`;
-      })
-      break;
-    }
-  }
-}
-
-connectedCallback() {
-  // Disable scroll.
-  document.body.style.overflow = 'hidden';
-
-  this.addEventListener('pointerdown', this.onPointerdown);
-  this.addEventListener('pointerleave', this.onPointerleave);
-  this.addEventListener('pointermove', this.onPointermove);
-  this.addEventListener('pointerup', this.onPointerup);
-  this.addEventListener('wheel', this.onWheel);
-}
-
-disconnectedCallback() {
-  this.removeEventListener('pointerdown', this.onPointerdown);
-  this.removeEventListener('pointerleave', this.onPointerleave);
-  this.removeEventListener('pointermove', this.onPointermove);
-  this.removeEventListener('pointerup', this.onPointerup);
-}
-
-onPointerdown(event) {
-  const { clientX, clientY } = event;
-  const x = Math.round(clientX);
-  const y = Math.round(clientY);
-
-  this.isDragging = true;
-  this.pointerCoordinates = { x, y };
-}
-
-onPointermove(event) {
-  const {
-    isDragging,
-    origin = { x: 0, y: 0 },
-    pointerCoordinates
-  } = this;
-  const { clientX, clientY } = event;
-  const x = Math.round(clientX);
-  const y = Math.round(clientY);
-
-  if (isDragging) {
-    // 1. Translate origin.
-    this.origin = {
-      x: origin.x - pointerCoordinates.x + x,
-      y: origin.y - pointerCoordinates.y + y
-    };
-    // 3. Apply to children nodes.
-    this.windowFrames.forEach((element) => {
-      element.style.left = `${this.origin.x}px`;
-      element.style.top = `${this.origin.y}px`;
-    })
-    // 3. Update pointer coordinates.
-    this.pointerCoordinates = { x, y };
-
-  }
-}
-
-onPointerleave() {
-  this.stopDragging();
-}
-
-onPointerup() {
-  this.stopDragging();
-}
-
-onWheel(event) {
-  this.scale = this.scale - event.deltaY * Math.pow(10, -InfinitePaper.scalePrecision);
-}
-
-stopDragging() {
-  this.isDragging = false;
-}
-
-get windowFrames() {
-  return this.querySelectorAll(':scope > window-frame');
-}
-
-get scale() {
-  return Number(this.getAttribute('scale')) || 1;
-}
-
-set scale(value) {
-  if (value > 0) {
-    this.setAttribute('scale', Number(value.toFixed(InfinitePaper.scalePrecision)));
-  }
-}
-});
+customElements.define("infinite-paper", InfinitePaper);
 
 /**
  * WindowFrame
  */
-customElements.define('window-frame', class WindowFrame extends HTMLElement {
-constructor() {
-  super();
+class WindowFrame extends HTMLElement {
+	constructor() {
+		super();
 
-  const template = document.createElement('template');
-  template.innerHTML = `
+		const template = document.createElement("template");
+		template.innerHTML = `
 <style>
   :host {
     box-shadow: 1px 1px 7px 1px var(
@@ -163,76 +171,82 @@ constructor() {
 <iframe frameborder="0"></iframe>
 `;
 
-  this.attachShadow({mode: 'open'}).appendChild(template.content.cloneNode(true));
+		this.attachShadow({mode: "open"}).appendChild(
+			template.content.cloneNode(true),
+		);
+	}
+
+	static get observedAttributes() {
+		return [
+			// position
+			"top",
+			"left",
+			// dimension
+			"width",
+			"height",
+			// iframe URL
+			"src",
+		];
+	}
+
+	attributeChangedCallback(name, oldValue, newValue) {
+		switch (name) {
+			case "top":
+			case "left": {
+				const num = Math.round(newValue);
+
+				if (typeof num === "number") {
+					this.style[name] = `${num}px`;
+				}
+
+				break;
+			}
+
+			case "width":
+			case "height": {
+				const {iframe, scale} = this;
+
+				const num = Math.round(newValue * scale);
+
+				if (typeof num === "number" && num >= 0) {
+					this.style[name] = `${num}px`;
+					iframe.style.transform = `scale(${scale})`;
+					iframe[name] = newValue;
+				}
+
+				break;
+			}
+
+			case "src": {
+				this.iframe.src = newValue;
+				break;
+			}
+		}
+	}
+
+	get paper() {
+		const {parentNode} = this;
+
+		if (parentNode) {
+			if (parentNode.tagName === "INFINITE-PAPER") {
+				return parentNode;
+			}
+		}
+	}
+
+	get iframe() {
+		return this.shadowRoot.querySelector("iframe");
+	}
+
+	get scale() {
+		const {paper} = this;
+
+		if (paper) {
+			return Number(paper.getAttribute("scale")) || 1;
+		} else {
+			return 1;
+		}
+	}
 }
 
-static get observedAttributes() {
-  return [
-    /* position */
-    'top', 'left',
-    /* dimension */
-    'width', 'height',
-    /* iframe URL */
-    'src'
-  ];
-}
-
-attributeChangedCallback(name, oldValue, newValue) {
-  switch (name) {
-    case 'top':
-    case 'left': {
-      const num = Math.round(newValue);
-
-      if (typeof num === 'number') {
-        this.style[name] = `${num}px`;
-      }
-
-      break;
-    }
-
-    case 'width':
-    case 'height': {
-      const { iframe, scale } = this;
-
-      const num = Math.round(newValue * scale);
-
-      if (typeof num === 'number' && num >= 0) {
-        this.style[name] = `${num}px`;
-        iframe.style.transform = `scale(${scale})`;
-        iframe[name] = newValue;
-      }
-
-      break;
-    }
-
-    case 'src': {
-      this.iframe.src = newValue;
-      break;
-    }
-  }
-}
-
-get paper() {
-  const { parentNode } = this;
-
-  if (parentNode) {
-    if (parentNode.tagName === 'INFINITE-PAPER') {
-      return parentNode;
-    }
-  }
-}
-
-get iframe() {
-  return this.shadowRoot.querySelector('iframe');
-}
-
-get scale() {
-  const { paper } = this;
-
-  if (paper) {
-    return Number(paper.getAttribute('scale')) || 1;
-  } else {
-    return 1;
-  }
-}
-});
+customElements.define("window-frame", WindowFrame);
