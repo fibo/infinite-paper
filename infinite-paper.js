2f8efc129e25f5473fa6c09cd8da16566faafad7
Gianluca Casati <fibo@users.noreply.github.com>
2025-07-20 14:53:09 +0700

    use html tag template

--- infinite-paper.js
+++ infinite-paper.js
@@ -1,30 +1,37 @@
+const html = (strings, ...expressions) => {
+	let template = document.createElement('template');
+	template.innerHTML = strings.reduce(
+		(result, string, index) => result + string + (expressions[index] ?? ''), ''
+	);
+	return template
+}
+
 /**
  * InfinitePaper
  */
+
+const infinitePaperTemplate = html`
+	<style>
+		:host {
+			background-color: var(--infinite-paper-background-color, #fefefe);
+			position: absolute;
+			left: 0;
+			top: 0;
+			width: 100vw;
+			height: 100vh;
+		}
+		::slotted(window-frame) {
+			position: absolute;
+		}
+	</style>
+	<slot></slot>
+`;
+
 class InfinitePaper extends HTMLElement {
 	constructor() {
 		super();
-
-		const template = document.createElement("template");
-		template.innerHTML = `
-<style>
-  :host {
-    background-color: var(--infinite-paper-background-color, #fefefe);
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 100vw;
-    height: 100vh;
-  }
-  ::slotted(window-frame) {
-    position: absolute;
-  }
-</style>
-<slot></slot>
-`;
-
-		this.attachShadow({mode: "open"}).appendChild(
-			template.content.cloneNode(true),
+		this.attachShadow({mode: 'open'}).appendChild(
+			infinitePaperTemplate.content.cloneNode(true)
 		);
 	}
 
@@ -32,16 +39,14 @@ class InfinitePaper extends HTMLElement {
 		return 3;
 	}
 
-	static get observedAttributes() {
-		return ["scale"];
-	}
+	static get observedAttributes() { return ['scale'] }
 
-	attributeChangedCallback(name, oldValue, newValue) {
+	attributeChangedCallback(name, _oldValue, newValue) {
 		switch (name) {
-			case "scale": {
+			case 'scale': {
 				this.windowFrames.forEach((element) => {
-					const wantedWidth = element.getAttribute("width");
-					const wantedHeight = element.getAttribute("height");
+					const wantedWidth = element.getAttribute('width');
+					const wantedHeight = element.getAttribute('height');
 
 					const width = Math.round(wantedWidth * newValue);
 					const height = Math.round(wantedHeight * newValue);
@@ -49,7 +54,7 @@ class InfinitePaper extends HTMLElement {
 					element.style.width = `${width}px`;
 					element.style.height = `${height}px`;
 
-					const iframe = element.shadowRoot.querySelector("iframe");
+					const iframe = element.shadowRoot.querySelector('iframe');
 					iframe.style.transform = `scale(${newValue})`;
 				});
 				break;
@@ -59,20 +64,20 @@ class InfinitePaper extends HTMLElement {
 
 	connectedCallback() {
 		// Disable scroll.
-		document.body.style.overflow = "hidden";
+		document.body.style.overflow = 'hidden';
 
-		this.addEventListener("pointerdown", this.onPointerdown);
-		this.addEventListener("pointerleave", this.onPointerleave);
-		this.addEventListener("pointermove", this.onPointermove);
-		this.addEventListener("pointerup", this.onPointerup);
-		this.addEventListener("wheel", this.onWheel);
+		this.addEventListener('pointerdown', this.onPointerdown);
+		this.addEventListener('pointerleave', this.onPointerleave);
+		this.addEventListener('pointermove', this.onPointermove);
+		this.addEventListener('pointerup', this.onPointerup);
+		this.addEventListener('wheel', this.onWheel);
 	}
 
 	disconnectedCallback() {
-		this.removeEventListener("pointerdown", this.onPointerdown);
-		this.removeEventListener("pointerleave", this.onPointerleave);
-		this.removeEventListener("pointermove", this.onPointermove);
-		this.removeEventListener("pointerup", this.onPointerup);
+		this.removeEventListener('pointerdown', this.onPointerdown);
+		this.removeEventListener('pointerleave', this.onPointerleave);
+		this.removeEventListener('pointermove', this.onPointermove);
+		this.removeEventListener('pointerup', this.onPointerup);
 	}
 
 	onPointerdown(event) {
@@ -129,86 +134,84 @@ class InfinitePaper extends HTMLElement {
 	}
 
 	get windowFrames() {
-		return this.querySelectorAll(":scope > window-frame");
+		return this.querySelectorAll(':scope > window-frame');
 	}
 
 	get scale() {
-		return Number(this.getAttribute("scale")) || 1;
+		return Number(this.getAttribute('scale')) || 1;
 	}
 
 	set scale(value) {
-		if (value > 0) {
-			this.setAttribute(
-				"scale",
-				Number(value.toFixed(InfinitePaper.scalePrecision)),
-			);
-		}
+		if (value <= 0) return;
+		this.setAttribute(
+			'scale',
+			Number(value.toFixed(InfinitePaper.scalePrecision))
+		);
 	}
 }
 
-customElements.define("infinite-paper", InfinitePaper);
+customElements.define('infinite-paper', InfinitePaper);
 
 /**
  * WindowFrame
  */
+
+const windowFrameTemplate = html`
+<style>
+	:host {
+		box-shadow: 1px 1px 7px 1px var(
+			--window-frame-box-shadow-color, rgba(0, 0, 0, 0.17)
+		);
+	}
+	:host > iframe {
+		transform-origin: 0px 0px;
+	}
+</style>
+
+<iframe frameborder='0'></iframe>
+`
+
 class WindowFrame extends HTMLElement {
 	constructor() {
 		super();
-
-		const template = document.createElement("template");
-		template.innerHTML = `
-<style>
-  :host {
-    box-shadow: 1px 1px 7px 1px var(
-      --window-frame-box-shadow-color, rgba(0, 0, 0, 0.17)
-    );
-  }
-  :host > iframe {
-    transform-origin: 0px 0px;
-  }
-</style>
-
-<iframe frameborder="0"></iframe>
-`;
-
-		this.attachShadow({mode: "open"}).appendChild(
-			template.content.cloneNode(true),
+		this.attachShadow({mode: 'open'}).appendChild(
+			windowFrameTemplate.content.cloneNode(true)
 		);
 	}
 
 	static get observedAttributes() {
 		return [
 			// position
-			"top",
-			"left",
+			'top',
+			'left',
 			// dimension
-			"width",
-			"height",
+			'width',
+			'height',
 			// iframe URL
-			"src",
+			'src',
 		];
 	}
 
-	attributeChangedCallback(name, oldValue, newValue) {
+	attributeChangedCallback(name, _oldValue, newValue) {
 		switch (name) {
-			case "top":
-			case "left": {
+			case 'top':
+			case 'left': {
 				const num = Math.round(newValue);
 
-				if (typeof num === "number") {
+				if (typeof num === 'number') {
 					this.style[name] = `${num}px`;
 				}
 
 				break;
 			}
 
-			case "width":
-			case "height": {
+			case 'width':
+			case 'height': {
 				const {iframe, scale} = this;
 
 				const num = Math.round(newValue * scale);
 
-				if (typeof num === "number" && num >= 0) {
+				if (typeof num === 'number' && num >= 0) {
 					this.style[name] = `${num}px`;
 					iframe.style.transform = `scale(${scale})`;
 					iframe[name] = newValue;
@@ -217,7 +220,7 @@ class WindowFrame extends HTMLElement {
 				break;
 			}
 
-			case "src": {
+			case 'src': {
 				this.iframe.src = newValue;
 				break;
 			}
@@ -228,25 +231,25 @@ class WindowFrame extends HTMLElement {
 		const {parentNode} = this;
 
 		if (parentNode) {
-			if (parentNode.tagName === "INFINITE-PAPER") {
+			if (parentNode.tagName === 'INFINITE-PAPER') {
 				return parentNode;
 			}
 		}
 	}
 
 	get iframe() {
-		return this.shadowRoot.querySelector("iframe");
+		return this.shadowRoot.querySelector('iframe');
 	}
 
 	get scale() {
 		const {paper} = this;
 
 		if (paper) {
-			return Number(paper.getAttribute("scale")) || 1;
+			return Number(paper.getAttribute('scale')) || 1;
 		} else {
 			return 1;
 		}
 	}
 }
 
-customElements.define("window-frame", WindowFrame);
+customElements.define('window-frame', WindowFrame);
